a8363bc496faa9a483b1ad3baaa4749ecf6b822b
kkopitehong <kkopite.hong@smartcorelink.com>
2023-10-09 18:17:29 +0800

    css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,10 +2,11 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Head from 'next/head'
 import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
     <Head> 
       <title>First Post</title>
     </Head>
@@ -26,6 +27,6 @@ export default function FirstPost() {
         width={144}
         alt="kkopite"
       />
-    </>
+    </Layout>
   )
 }
